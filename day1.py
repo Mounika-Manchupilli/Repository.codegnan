170d64bc6b91b6b3e19301f8599e5be5b14c796d
Mounika-Manchupilli <mounikamanchupilli@gmail.com>
2026-04-16 10:53:00 +0530

    I started my day1 class at codegnan

---
+++ day1.py
@@ -0,0 +1,65 @@
+'''print("This is my first class at codegnan")
+What is python?
+-->Python is a high-level language means no need to worry about low level management like memory allocation'''
+a=0
+b=9
+print(id(a))
+print(if(b))
+-->Python never save anything perminantly, it will only save at run time
+
+-->Python supports OOPS
+
+
+a = int(input("enter a num: "))
+b = int(input("enter a num: "))
+print(a + b)
+print(f"b = {b}")
+print(f"a = {a}")
+
+-->This is also called dynamically- typed language means no need to mention the type of data your take in a variable.
+
+
+
+a = 9
+b = "Python"
+print(type(a))
+print(type(b))
+
+-->Python is also called as interpered language means the python code will executes line by line. 
+-->It is a syntax less language
+a = 9
+ b = "Python"
+print(type(a))
+print(type(b))
+
+
+
+'''variables
+----------
+To define a variable, we have 2 rules
+1.Good way to define a variable(No error)
+2.Bad way to define a variable (will get error)'''
+
+A = 90
+print(A)
+
+_v = 90
+print(_v)
+
+
+&num = 90
+
+Note:-
+-->We are going to use meaningful words or name for defining variables
+num = 90
+num_2= 60
+
+Keywords
+-------
+-->This keywords not gonna use as a variable
+
+
+
+
+
+
